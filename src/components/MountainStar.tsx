b7227611b5ca21c269224ae5c14ab92331b75abb
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-11-28 00:49:34 -0800

    Welcome to Earth

--- src/components/MountainStar.tsx
+++ src/components/MountainStar.tsx
@@ -24,10 +24,10 @@ const Moon = styled.img<{ $offset: number }>`
     height: 20vh;
     z-index: 1;
     position: absolute;
-    left: 500px;
+    left: 350px;
     top: 60vh;
-    transform: translateY(${props => props.$offset * -0.25}px);
-    transform: translateX(${props => props.$offset * 0.5}px);
+    transform: translateY(${props => props.$offset * -0.3}px);
+    transform: translateX(${props => props.$offset * 0.7}px);
 `;
 
 const Mountain = styled.img<{ $offset: number }>`
@@ -35,8 +35,19 @@ const Mountain = styled.img<{ $offset: number }>`
     width: 100%;
     z-index: 2;
     position: absolute;
-    transform: translateY(${props => props.$offset * 0.5}px);
+    transform: translateY(${props => props.$offset * 0.7}px);
 `;
+
+const Welcome =styled.div <{ $offset: number }>`
+    transform: translateY(${props => props.$offset * 0.6}px);
+    transform: translateX(${props => props.$offset * -0.7}px);
+    color: white;
+    font-size: 300px;
+    position: absolute;
+    left: 350px;
+    top: 60vh;
+    z-index: 0.8;
+`
 
 function MountainStar() {
     const [offSetY, setOffSetY] = useState(0);
@@ -52,6 +63,8 @@ function MountainStar() {
             <Contain>
                 <Moon src={moon} $offset={offSetY} />
                 <Mountain src={mountain} $offset={offSetY} />
+                <Welcome $offset={offSetY}> Welcome to Earth </Welcome>
+            
             </Contain>
             <EarthPage />
         </Stars>
