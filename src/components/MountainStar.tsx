ee40290b29de135dbc2a4aac396a10ad30a2414a
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-11-27 23:25:10 -0800

    parallax effect, primary

--- src/components/MountainStar.tsx
+++ src/components/MountainStar.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import twinklestar from "./../media/twinklestar.svg";
 import mountain from "./../media/mountain.png";
 import moon from "./../media/moon.svg";
 import EarthPage from "./EarthPage";
 
-const Contain =styled.div` 
+const Contain = styled.div` 
     min-height: 400vh;
 `
 const Stars = styled.div`
     background-image: url(${twinklestar});
     min-height: 1000vh; 
-    width: 2000px;
+    width: 100%;
     background-attachment: fixed;
     background-position: center;
     background-repeat: no-repeat;
@@ -19,28 +19,34 @@ const Stars = styled.div`
     z-index: 0;
 `
 const Moon = styled.img`
-    height: 50vh;
+    height: 20vh;
     z-index: 1;
-    position: relative;
+    position: absolute;
     left: 70px;
-    top: 80vh;
+    top: 70vh;
 `
 const Mountain = styled.img`
     height: 100vh;
-    width: 2000px;
+    width: 100%;
     z-index: 2;
-    position: fixed;
-    transform: translate(-50%, -50%);   
+    position: absolute;  
 `
 function MountainStar() {
-    return(
-            <Stars>
-                <Contain>
-                    <Moon src={moon}/>
-                    <Mountain src={mountain} />
-                </Contain>
-                <EarthPage />
-            </Stars>
+    const [offSetY, setOffSetY] = useState(0);
+    const handlescroll = () => setOffSetY(window.pageYOffset);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handlescroll);
+        return () => window.removeEventListener('scroll', handlescroll);
+    }, []);
+    return (
+        <Stars>
+            <Contain>
+                <Moon src={moon} style={{ transform: `translateY(${offSetY * -0.5}px)` }} />
+                <Mountain src={mountain} />
+            </Contain>
+            <EarthPage />
+        </Stars>
 
 
     );
