e2f77607be91f06dfedf2b68246273f8674ba1a1
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-12-06 14:34:35 -0800

    Diffrential from image and background

--- src/components/ArtImg.tsx
+++ src/components/ArtImg.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ArtImg = styled.img`
-    height: 300px;
-    width: 300px;
+    height: 400px;
+    width: 400px;
+    object-fit: cover;
+    transform-origin: center;
+    transform: perspective(800px) rotateY(25deg);
+    transition: 0.5s;
+    padding: 200px;
+    --webkit-box-reflect: below 2px linear-gradient(transparent, transparent, #0004);    
+    :hover{
+        transform: perspective(800px);       
+        transform: rotateY(0deg);
+    }
 `
 interface ArtImageProps {
     image: string;
--- src/components/EarthGallary.tsx
+++ src/components/EarthGallary.tsx
@@ -5,38 +5,17 @@ import { earthData } from './configEarthGallary';
 import { Link } from 'react-router-dom';
 
 const Body = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: grid;
-    box-sizing: border-box; 
-    place-items:center;
-     ;
-`
-
-const Container = styled.div`
-    max-width: 600px;
-    max-height: 350px;
+    width: 400vh;
+    height: 00vh;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
-    :hover{
-        opacity:0.3;
-    } 
+    opacity: 0.8;
+    z-index: 7;
+ 
 `
 
 const Card = styled.div`
-    width: 30%;
-    height: 100%;
-    object-fit: cover;
-    transform-origin: center;
-    transform: perspective(800px) rotateY(25deg);
-    transition: 0.5s;
-    --webkit-box-reflect: below 2px linear-gradient(transparent, transparent, #0004);    
-    :hover{
-        transform: perspective(800px);       
-        transform: rotateY(0deg);
-         opacity:1;
+   :hover{
+         opacity:0.8;
     }
 `
 
@@ -44,7 +23,6 @@ export default function EarthGallary() {
     return (
     <Link to="EarthGallary" >   
         <Body>
-            <Container>
                 <Card>
                     {
                     earthData.map(({ image }, index) => {
@@ -52,7 +30,6 @@ export default function EarthGallary() {
                     })
                 }   
                 </Card>
-            </Container>
         </Body>
     </Link>    
     );
