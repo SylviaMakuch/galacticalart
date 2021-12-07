fa1c2f2d1ca69646c8b218426f971e9b89b4d147
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-12-07 09:15:32 -0800

    ScrollToTop Added

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import EnterAnimation from "./components/Animation/EnterAnimation";
 import MountainStar from "./components/MountainStar";
 import SpacePageI from "./components/SpacePageI";
 import EarthGallary from "./components/EarthGallary";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
     return (
         <div>
             <BrowserRouter>
+                <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<EnterAnimation />} />
                     <Route path="/main" element={<MainPage />} />
                     <Route path="/space" element={<SpacePageI />} />
-                    <Route path="/earth" element ={<MountainStar />} />
-                    <Route path="/earthgallary" element ={<EarthGallary />} />
+                    <Route path="/earth" element={<MountainStar />} />
+                    <Route path="/earthgallary" element={<EarthGallary />} />
                 </Routes>
             </BrowserRouter>
         </div>
